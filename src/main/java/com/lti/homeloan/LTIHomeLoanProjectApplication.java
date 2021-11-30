002b4c9198a2291e3494c32c71a782b99a32e098
abspecial <ashutoshbarde6@gmail.com>
2021-11-30 15:08:15 +0530

    initial commit

---
+++ src/main/java/com/lti/homeloan/LTIHomeLoanProjectApplication.java
@@ -0,0 +1,14 @@
+package com.lti.homeloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LTIHomeLoanProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LTIHomeLoanProjectApplication.class, args);
+		System.out.println("Welcome to SpringBoot App");
+	}
+
+}
